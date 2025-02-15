fc03a9b1be40190178db55dfabcc1da086b8d473
Jess Nunes <eng.diias@gmail.com>
2025-02-15 08:17:16 -0300

    Inicializacao Projeto

---
+++ src/main/java/com/devsuperior/exm02/Exm02Application.java
@@ -0,0 +1,14 @@
+package com.devsuperior.exm02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exm02Application {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(Exm02Application.class, args);
+	}
+
+}
